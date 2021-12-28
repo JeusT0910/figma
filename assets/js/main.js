8881f24d47df3f94a11bb602357acda1ceae21fa
JeusT0910 <72457937+JeusT0910@users.noreply.github.com>
2021-12-28 11:31:36 +0700

    update 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,62 @@ var closee = document.querySelector('.close');
 closee.addEventListener('click', function(){
     imgFix.classList.remove('dp-block');
 })
+function hienthi(id, name){
+    $(`#${name}`).toggle('slow');
+    $(`.cong${id}`).toggleClass('dp-none');
+    $(`.tru${id}`).toggleClass('dp-none');
+}
+$('.menu__link > a i').click(function(e){
+    e.preventDefault();
+})
+
+// auto slideshow
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slideshow");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000);
+}
+// click slideshow
+var leftNode = document.getElementById('left');
+var rightNode = document.getElementById('right');
+var itemNode = document.getElementsByClassName('incredible__item');
+console.log(itemNode)
+var l = 0 ;
+rightNode.onclick = () => {
+    console.log('cong')
+    l++;
+    console.log(l)
+    for(var i of itemNode) {
+        if(l==0) {i.style.left ="0px";}
+        if(l==1) {i.style.left ="-300px";}
+        if(l==2) {i.style.left ="-600px";}
+        if(l==3) {i.style.left ="-900px";}
+        if(l==4) {i.style.left ="-1200px";}
+        if(l>4) {l=4;}
+    }
+}
+leftNode.onclick = () => {
+    console.log('tru')
+    console.log(l)
+    l--;
+    for(var i of itemNode){
+        if(l==0) {i.style.left ="0px";}
+        if(l==1) {i.style.left ="-300px";}
+        if(l==2) {i.style.left ="-600px";}
+        if(l==3) {i.style.left ="-900px";}
+        if(l<0) {l=0;}
+    }
+}
